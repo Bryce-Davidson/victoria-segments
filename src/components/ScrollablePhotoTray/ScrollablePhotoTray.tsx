906bb6fd303737a11b1f936865ffd2049b6fb988
Bryce-Davidson <lifebryce@gmail.com>
2023-02-20 00:00:13 -0800

    Added no idea how to use display inline block etc

--- src/components/ScrollablePhotoTray/ScrollablePhotoTray.tsx
+++ src/components/ScrollablePhotoTray/ScrollablePhotoTray.tsx
@@ -1,16 +1,28 @@
 import Image from "next/image";
 import photo_map from "../../data/photo_map";
 import styles from "./ScrollablePhotoTray.module.css";
+import { useEffect, useState } from "react";
 
 function ScrollblePhotoTray() {
+  const [open, setOpen] = useState(true);
+
   return (
     <div className={styles.media_scroller}>
+      {/* <button
+        onClick={(event) => {
+          event.preventDefault();
+          setOpen(!open);
+          console.log(open);
+        }}
+      >
+        OPen
+      </button> */}
       {photo_map.features.map((photo, index) => {
         return (
           <div key={index} className={styles.media_element}>
             <Image
-              width={640}
-              height={360}
+              width={1280}
+              height={720}
               alt="oops"
               src={"/photos/" + photo?.properties?.photo_name}
             />
