d27fb667213ce5dbb39596b4559b02d94c383220
Bryce-Davidson <lifebryce@gmail.com>
2023-02-20 01:39:46 -0800

    Added a popup button for the photos

--- src/components/ScrollablePhotoTray/ScrollablePhotoTray.tsx
+++ src/components/ScrollablePhotoTray/ScrollablePhotoTray.tsx
@@ -6,20 +6,39 @@ import { useEffect, useState } from "react";
 function ScrollblePhotoTray() {
   const [open, setOpen] = useState(true);
 
+  let string = open ? "px-4 py-1" : "p-5";
+
   useEffect(() => {
     console.log(open);
   }, [open]);
 
   return (
-    <div>
+    <div className="fixed bottom-0 flex w-screen flex-col items-center justify-center">
       <button
-        className="fixed top-0 bg-slate-200 p-10"
+        className={"rounded-t-md bg-white " + string}
         onClick={(event) => {
           event.preventDefault();
           setOpen(!open);
         }}
       >
-        {open ? "Close" : "Open"} Photos
+        {open ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        ) : (
+          "Photos"
+        )}
       </button>
 
       <div
