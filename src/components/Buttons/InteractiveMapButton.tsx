f617326ab69b9dc651e78a3584cc55e5aa1a649a
Bryce-Davidson <lifebryce@gmail.com>
2023-03-28 15:00:02 -0700

    Changed interactive map button text

--- src/components/Buttons/InteractiveMapButton.tsx
+++ src/components/Buttons/InteractiveMapButton.tsx
@@ -8,7 +8,7 @@ export default function InteractiveMapButton({ href }: { href: string }) {
       className="absolute bottom-8
   left-1/2 flex -translate-x-1/2 items-center gap-4 rounded-md bg-white py-3 px-6"
     >
-      <h4 className="order-2 text-center">Interactive Map</h4>
+      <h4 className="order-2 text-center">Map</h4>
       <ZoomOutMapIcon className="order-1" />
     </Link>
   );
